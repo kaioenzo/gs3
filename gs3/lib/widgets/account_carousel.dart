1e0dec51522a9f0a82d05550dee8a13efb4b232f
kaioenzo <kaioenzobr@gmail.com>
2024-05-29 00:20:45 -0300

    feat: adiciona padding no account_carousel e arruma gradient

--- gs3/lib/widgets/account_carousel.dart
+++ gs3/lib/widgets/account_carousel.dart
@@ -59,138 +59,142 @@ class _AccountCarouselState extends State<AccountCarousel> {
   @override
   Widget build(BuildContext context) {
     final accountProvider = Provider.of<AccountProvider>(context);
-    return SizedBox(
-      height: 200,
-      child: PageView.builder(
-        controller: pageController,
-        onPageChanged: (page) {
-          accountProvider.selectAccount(page);
-        },
-        scrollDirection: Axis.horizontal,
-        itemCount: accountProvider.accounts.length,
-        itemBuilder: (ctx, index) {
-          final account = accountProvider.accounts[index];
-          return GestureDetector(
-            onTap: () {
-              pageController.animateToPage(
-                index,
-                duration: const Duration(milliseconds: 300),
-                curve: Curves.easeInOut,
-              );
-              accountProvider.selectAccount(index);
-            },
-            child: SizedBox(
-              width: 300,
-              height: 140,
-              child: Container(
-                margin: const EdgeInsets.only(right: 15),
-                decoration: BoxDecoration(
-                  gradient: getCustomGradient(index),
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: Card(
-                  color: Colors.transparent,
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Container(
-                              width: 88,
-                              height: 56,
-                              decoration: const BoxDecoration(
-                                color: Colors.white,
-                              ),
-                            ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  formatCardNumber(account.creditCardNumber),
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                  ),
-                                ),
-                                Text(
-                                  account.name,
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 14.0,
-                                    fontWeight: FontWeight.w400,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            IconButton(
-                              color: const Color.fromRGBO(8, 158, 227, 1),
-                              onPressed: () {
-                                setState(() {
-                                  showCardInfo = !showCardInfo;
-                                });
-                              },
-                              icon: Icon(showCardInfo ? Icons.visibility_off : Icons.visibility),
-                            )
-                          ],
-                        ),
-                        const Padding(
-                          padding: EdgeInsets.symmetric(vertical: 12.0),
-                          child: Divider(
-                            color: Color.fromRGBO(54, 96, 161, 1),
-                            thickness: 1.0,
-                          ),
-                        ),
-                        Expanded(
-                          child: Row(
+    return Padding(
+      padding: const EdgeInsets.only(top: 16.0),
+      child: SizedBox(
+        height: 200,
+        child: PageView.builder(
+          controller: pageController,
+          onPageChanged: (page) {
+            accountProvider.selectAccount(page);
+          },
+          scrollDirection: Axis.horizontal,
+          itemCount: accountProvider.accounts.length,
+          itemBuilder: (ctx, index) {
+            final account = accountProvider.accounts[index];
+            return GestureDetector(
+              onTap: () {
+                pageController.animateToPage(
+                  index,
+                  duration: const Duration(milliseconds: 300),
+                  curve: Curves.easeInOut,
+                );
+                accountProvider.selectAccount(index);
+              },
+              child: SizedBox(
+                width: 300,
+                height: 140,
+                child: Container(
+                  margin: const EdgeInsets.only(right: 15),
+                  decoration: BoxDecoration(
+                    gradient: getCustomGradient(index),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Card(
+                    color: Colors.transparent,
+                    elevation: 0,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            crossAxisAlignment: CrossAxisAlignment.center,
                             children: [
+                              Container(
+                                width: 88,
+                                height: 56,
+                                decoration: const BoxDecoration(
+                                  color: Colors.white,
+                                ),
+                              ),
                               Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  const Text(
-                                    'Limite disponível',
-                                    style: TextStyle(
+                                  Text(
+                                    formatCardNumber(account.creditCardNumber),
+                                    style: const TextStyle(
                                       color: Colors.white,
-                                      fontSize: 10.0,
                                     ),
                                   ),
                                   Text(
-                                    'R\$ ${account.balanceAmmount.toStringAsFixed(2)}',
+                                    account.name,
                                     style: const TextStyle(
-                                        color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.w700),
-                                  ),
-                                ],
-                              ),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.end,
-                                children: [
-                                  const Text(
-                                    'Melhor dia de compra',
-                                    style: TextStyle(
                                       color: Colors.white,
-                                      fontSize: 10.0,
+                                      fontSize: 14.0,
+                                      fontWeight: FontWeight.w400,
                                     ),
                                   ),
-                                  Text(
-                                    account.betterDayToBuy,
-                                    style: const TextStyle(
-                                        color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.w700),
-                                  ),
                                 ],
                               ),
+                              IconButton(
+                                color: const Color.fromRGBO(8, 158, 227, 1),
+                                onPressed: () {
+                                  setState(() {
+                                    showCardInfo = !showCardInfo;
+                                  });
+                                },
+                                icon: Icon(showCardInfo ? Icons.visibility_off : Icons.visibility),
+                              )
                             ],
                           ),
-                        )
-                      ],
+                          const Padding(
+                            padding: EdgeInsets.symmetric(vertical: 12.0),
+                            child: Divider(
+                              color: Color.fromRGBO(54, 96, 161, 1),
+                              thickness: 1.0,
+                            ),
+                          ),
+                          Expanded(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Text(
+                                      'Limite disponível',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 10.0,
+                                      ),
+                                    ),
+                                    Text(
+                                      'R\$ ${account.balanceAmmount.toStringAsFixed(2)}',
+                                      style: const TextStyle(
+                                          color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.w700),
+                                    ),
+                                  ],
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  children: [
+                                    const Text(
+                                      'Melhor dia de compra',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 10.0,
+                                      ),
+                                    ),
+                                    Text(
+                                      account.betterDayToBuy,
+                                      style: const TextStyle(
+                                          color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.w700),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
     );
   }
